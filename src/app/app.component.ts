d7f4ab663de82519b58d982bf2076211213ca137
masaki <masaki.iima@gmail.com>
2017-06-02 14:40:47 +0900

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Hero } from './hero';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  body = 'angular';
+
+  heroes: Hero[] = [
+    {id: 1, name: 'Mr.Nice'},
+    {id: 2, name: 'Narco'},
+    {id: 3, name: 'Bombasto'},
+  ];
+}
+
+
